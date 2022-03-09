373cc6409088945372d31f1a372b35e88b095d0b
Olle Ronstad <Olle.Ronstad@gmail.com>
2022-03-09 19:38:39 +0100

    add coments to document code

--- model.rb
+++ model.rb
@@ -1,13 +1,16 @@
 require 'bcrypt'
 require 'sqlite3'
 
-#creates the user with the specified data and returns the users id
+
+#load the database and sets reasult as hash flag to true
 def load_db()
     db = SQLite3::Database.new('DB/DataBase.db')
     db.results_as_hash = true
     db
 end
 
+#return true or false according to if the user is has the reqierd autoriztaion
+#TODO make so it updats last logging attmept and such
 def auth(owner_id,req_auth_level,user_id,user_passwd)
     return true
     db = load_db()
@@ -19,16 +22,17 @@ def auth(owner_id,req_auth_level,user_id,user_passwd)
     end
 end
 
-
+#return data about the user
 def get_user(id)
     return load_db().execute("SELECT user_name,first_name,last_name,admin_level FROM users WHERE id=?",id)[0]
 end
 
-
+#return data about the user wich is demed public, such as username and firstname 
 def get_user_pub(id)
-    return load_db().execute("SELECT user_name,first_name,last_name,admin_level FROM users WHERE id=?",id)[0]
+    return load_db().execute("SELECT user_name,first_name FROM users WHERE id=?",id)[0]
 end
 
+#creates the user with the specified data and returns the users id
 def create_user(user_name, first_name, last_name, passwd)
     db = SQLite3::Database.new('DB/DataBase.db')
     if db.execute("SELECT * FROM users WHERE first_name=? AND last_name=?",first_name,last_name).empty?
