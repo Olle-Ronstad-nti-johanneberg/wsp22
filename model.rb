6d3d0d82071c7b5e69017f6e2716f357f4a92d65
Olle Ronstad <olle.ronstad@gmail.com>
2022-03-23 15:56:47 +0100

    move auth to own file

--- model.rb
+++ model.rb
@@ -9,20 +9,6 @@ def load_db()
     db
 end
 
-#return true or false according to if the user is has the reqierd autoriztaion
-#TODO make so it updats last logging attmept and such
-def auth(owner_id,req_auth_level,user_id,user_passwd)
-    return true
-    db = load_db()
-    if owner_id == user_id
-        user_hash db.execute("SELECT paswd_hash FROM users WHERE id=?",owner_id)[0]["id"]
-        return BCrypt::passwd.new(user_hash["paswd_hash"]) == user_passwd
-    else
-        user_hash = db.execute("SELECT paswd_hash FROM users WHERE id=?",owner_id)[0]
-        return (user_hash["admin_level"] >= req_auth_level) && BCrypt::passwd.new(user_hash["paswd_hash"]) == user_passwd
-    end
-end
-
 #return user id based on first_name and last_name
 def get_user_id(first_name,last_name)
     x = load_db().execute("SELECT id FROM users WHERE first_name=? AND last_name=?",first_name,last_name)[0]
