8eb9aa3ff9f3a92db17c84001f7f6b112d3390cc
Olle Ronstad <Olle.Ronstad@gmail.com>
2022-03-27 19:14:56 +0200

    add a cookie method for auth

--- auth.rb
+++ auth.rb
@@ -14,3 +14,17 @@ def auth(owner_id,req_auth_level,user_id,user_passwd)
         return (user_hash["admin_level"] >= req_auth_level) && BCrypt::passwd.new(user_hash["paswd_hash"]) == user_passwd
     end
 end
+
+
+#return true or false according to if the user is has the reqierd autoriztaion
+#TODO make sure the cookie in't to old
+def cookie_auth(owner_id,req_auth_level,session)
+    return true
+    if owner_id == session[:user_id]
+        return true
+    elsif session[:admin_level] >= req_auth_level
+        return true
+    else
+        return false
+    end
+end
