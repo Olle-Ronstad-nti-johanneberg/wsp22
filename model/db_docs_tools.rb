403d6472bc3c033fe7d60ed5a1c0f62746ff5802
Olle Ronstad <olle.ronstad@gmail.com>
2022-03-30 15:30:16 +0200

    add create docs method

---
+++ model/db_docs_tools.rb
@@ -0,0 +1,10 @@
+def create_docs(head,body,source)
+    db = load_db()
+    date = Time.new.to_s,
+    db.execute("INSERT INTO doc (date, head, body, source) VALUES (?,?,?,?)",
+    date
+    head,
+    body,
+    source)
+    return db.execute("SELECT id FROM docs WHERE date=? AND head=?",date, head)[0]["id"]
+end
