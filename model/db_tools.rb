47484aeeae36d27638cd62348077370982ea9c1e
Olle Ronstad <Olle.Ronstad@gmail.com>
2022-04-24 18:07:56 +0200

    add doc to db_tools

--- model/db_tools.rb
+++ model/db_tools.rb
@@ -1,6 +1,13 @@
 require 'sqlite3'
+#
+# A module with basic SQLite 3 database methods
+#
 module DBTools
-  # load the database and sets reasult as hash flag to true
+  #
+  # returns an SQLite3::Database object with the result as hash flag set to true
+  #
+  # @return [SQLite3::Database] The SQLite3 Database object
+  #
   def load_db
     db = SQLite3::Database.new('DB/DataBase.db')
     db.results_as_hash = true
