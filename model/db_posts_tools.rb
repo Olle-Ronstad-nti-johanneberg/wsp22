03831120a8263ab198a2fc73f3298c03a90a1d39
Olle Ronstad <Olle.Ronstad@gmail.com>
2022-04-24 17:36:14 +0200

    align method arguments

--- model/db_posts_tools.rb
+++ model/db_posts_tools.rb
@@ -15,11 +15,11 @@ module DBPostTools
     db = load_db
     date = Time.new.to_s
     db.execute('INSERT INTO post (date, head, body, user)
-              VALUES (?,?,?,?)',
-              date,
-              head,
-              body,
-              session[:user_id])
+               VALUES (?,?,?,?)',
+               date,
+               head,
+               body,
+               session[:user_id])
     id = db.last_insert_row_id
     create_post_doc_link(id, doc_link_ids)
     id
@@ -35,9 +35,9 @@ module DBPostTools
   def get_post_by_id(id)
     db = load_db
     db.execute('SELECT date, head, body, user
-              FROM post
-              WHERE id = ?',
-              id).first
+               FROM post
+               WHERE id = ?',
+               id).first
   end
 
   #
@@ -50,11 +50,11 @@ module DBPostTools
   def get_doc_links_from_post_id(id)
     db = load_db
     db.execute('SELECT doc.id, doc.head
-              FROM doc
-              INNER JOIN doc_post_rel
-              ON doc_post_rel.doc = doc.id
-              WHERE doc_post_rel.post = ?',
-              id)
+               FROM doc
+               INNER JOIN doc_post_rel
+               ON doc_post_rel.doc = doc.id
+               WHERE doc_post_rel.post = ?',
+               id)
   end
 
   #
@@ -82,9 +82,9 @@ module DBPostTools
   def delete_post_links(id)
     db = load_db
     db.execute('DELETE
-              FROM doc_post_rel
-              WHERE post = ?',
-              id)
+               FROM doc_post_rel
+               WHERE post = ?',
+               id)
   end
 
   #
@@ -98,12 +98,12 @@ module DBPostTools
     db = load_db
     date = Time.new.to_s
     db.execute('UPDATE post
-              SET head = ?,body = ?, date = ?
-              WHERE id = ?',
-              head,
-              body,
-              date,
-              id)
+               SET head = ?,body = ?, date = ?
+               WHERE id = ?',
+               head,
+               body,
+               date,
+               id)
     delete_post_links(id)
     create_post_doc_link(id, doc_link_ids)
   end
